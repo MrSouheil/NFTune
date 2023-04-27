373b091a61a501b418d7cfa76e83949a73fde489
MohammadItani11 <82660032+MohammadItani11@users.noreply.github.com>
2023-04-27 17:21:15 +0300

    last commit

--- Test Projects/NFT-Marketplace/src/components/Explore.js
+++ Test Projects/NFT-Marketplace/src/components/Explore.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import Card from './basic/Cards/Card';
 import Sheet from './basic/sheet/Sheet';
+import Text from './basic/text/Text';
+import Grid from './basic/grid/Grid';
 
 const Explore = () => {
   return (
-    <section>
+    <section className='relative'>
         <Sheet>
+        <Text type={'title'} className={'text-white mx-auto w-fit '}>Exclusive this week!</Text>
+        <Text className={'w-fit text-white text-center mx-auto mb-[20px]'} type={'subTitle'}>Check now the top trending NFT songs for this week!<br/>Discover a new way to own and experience music with our exclusive NFT songs</Text>
+        <Grid className={'gap-10'}>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+        </Grid>
+                
+          {/* 
             <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 h-[450px] gap-20'>
                 <Card></Card>
                 <Card></Card>
                 <Card className={'sm:max-lg:col-span-2'}></Card>
             </div>
+            */}
         </Sheet>
     </section>
   )
