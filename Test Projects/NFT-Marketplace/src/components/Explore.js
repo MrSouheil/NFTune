4b12e8408fa91ad628a77aa10735ded940c2d39a
MohammadItani11 <82660032+MohammadItani11@users.noreply.github.com>
2023-05-03 15:26:39 +0300

    Explore Cards update

--- Test Projects/NFT-Marketplace/src/components/Explore.js
+++ Test Projects/NFT-Marketplace/src/components/Explore.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Card from './basic/Cards/Card';
 import Sheet from './basic/sheet/Sheet';
 import Text from './basic/text/Text';
 import Grid from './basic/grid/Grid';
 
 const Explore = () => {
+  const [loadMore,setLoadMore]=useState(false);
   return (
     <section className='relative'>
         <Sheet>
         <Text type={'title'} className={'text-white mx-auto w-fit '}>Exclusive this week!</Text>
         <Text className={'w-fit text-white text-center mx-auto mb-5'} type={'subTitle'}>Check now the top trending NFT songs for this week!<br/>Discover a new way to own and experience music with our exclusive NFT songs</Text>
         <Grid className={'gap-20'}>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
+            <Card disabled='true' className={`w-[373px] h-[434px] place-self-center`}></Card>
+            <Card className='h-[556px]'></Card>
+            <Card disabled='true' className={`w-[373px] h-[434px] place-self-center`}></Card>
            
         </Grid>
         <Text type={'title'} className={'text-white mx-auto w-fit  pt-20 '}>Explore our NFT collection!</Text>
@@ -26,6 +27,19 @@ const Explore = () => {
                 <Card></Card>
                 <Card></Card>
                 <Card ></Card>
+                {!loadMore? <div onClick={()=>{setLoadMore(true)}} className='lg:col-span-3 md:col-span-2 flex flex-col gap-[5px] justify-center items-center'>
+                  <div className='flex gap-[20px]'><div className='w-[15px] h-[15px] rounded-[50%] bg-[#e4e7f1]'></div><div className='w-[15px] h-[15px] rounded-[50%] bg-[#e4e7f1]'></div><div className='w-[15px] h-[15px] rounded-[50%] bg-[#e4e7f1]'></div> </div>
+                  <Text type='text' >Load More</Text>
+                </div>:
+                <>
+                <Card></Card>
+                <Card></Card>
+                <Card ></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card ></Card>
+                </>
+                }
             </div>
         </Sheet>
     </section>
--- Test Projects/NFT-Marketplace/src/components/basic/Cards/Card.js
+++ Test Projects/NFT-Marketplace/src/components/basic/Cards/Card.js
@@ -6,10 +6,11 @@ import Button from '../button/Button';
 import Text from '../text/Text';
 
 const Card = (props) => {
+    const {disabled,price='unknown',NftName='unknown',Artist='unknown',Duration='unknown'}=props
   return (
     <div className={`relative overflow-hidden rounded-[20px] ${props.className} drop-shadow-[-16px_4px_9px_rgba(0,0,0,0.25)] h-[453px] bg-[#EEEEEE66]`}>
        {/*} <img className='h-full w-full' src={'https://tse2.mm.bing.net/th?id=OIP.kRSxqqns41UFBXWO3_q-iQHaHa&pid=Api&P=0'} alt={'token image'}></img>*/}
-        <div className='absolute bottom-0 h-[35%] bg-[rgb(255 255 255 / 60%)] w-full rounded-[20px] bg-white/[.6]'>
+        <div  className={`absolute bottom-0 h-[35%] ${disabled?'bg-[#ebebea40] hover:bg-[rgb(255 255 255 / 60%)':'bg-[rgb(255 255 255 / 60%)]'} hover:bg-[rgb(255 255 255 / 60%)] w-full rounded-[20px] bg-white/[.6]`}>
             <div className='w-[90%] mx-auto h-full relative'>
                 <div className='absolute -translate-y-1/2 flex'>
                     <img alt={'profile'} src={profile1}></img>
@@ -18,13 +19,13 @@ const Card = (props) => {
                 </div>
                 <div className='flex justify-between h-full items-center'>
                     <div>
-                        <Text type={'text'}>NFT:</Text>
-                        <Text type={'text'}>Artist:</Text>
-                        <Text type={'text'}>Duration:</Text>
+                        <Text type={'text'}>NFT: {NftName}</Text>
+                        <Text type={'text'}>Artist: {Artist}</Text>
+                        <Text type={'text'}>Duration: {Duration}</Text>
                     </div>
                     <div className='flex flex-col gap-[1rem]'>
-                        <Button bgColor={'#2EE09A'}>Buy Now</Button>
-                        <Button>More</Button>
+                        <Button bgColor={`${disabled?'#ACACAC':'#2EE09A'}`}>Buy Now</Button>
+                        <Button bgColor={`${disabled?'#ACACAC':'#426AF2'}`} >{price}</Button>
                     </div>
                 </div>
             </div>
