d878c53cfd6ff16d8f80ad1e7d566cac286cfe77
MrSouheil <souheiltawil750@gmail.com>
2023-07-28 19:50:25 +0300

    Repository Cleaning
    Removed unnecessary folders, test projects, moved the main project to main directory, cleaned empty/useless files.

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/eaVx_6XX9kbYA3kJv8OUIHRRUK0CHHWk",
+      accounts: ["78ed9f895ff391164a8f20caa9b0d525fb112bcfa2d4cf76d9b75d1b70b7595b"]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
